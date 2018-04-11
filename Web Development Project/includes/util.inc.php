0215c765c2e2a99f23e03a4daa55f225db278b2d
RomainDessaint <romaindessaint@gmail.com>
2018-04-11 18:50:20 +0200

    caméléon portugais

--- Web Development Project/includes/util.inc.php
+++ Web Development Project/includes/util.inc.php
@@ -1,5 +1,7 @@
 <?php
 
+////////////////////////////            ETUDIANTS          ////////////////////////////
+
 //Affichage d'un formulaire de connexion pour un étudiant
 function formLoginStudent() {
     $retour = '<form action="#" method = "post">';
@@ -20,7 +22,8 @@ function formLoginStudent() {
 
 //Authentification d'un étudiant
 function loginStudent() {
-    $lignes = file("../ressources/login_etudiant.csv");
+    $temp = "";
+    $lignes = file("../ressources/login_student.csv");
     $i=0;
     if (isset($_POST['submit'])) {
         $name = $_POST['name'];
@@ -30,17 +33,70 @@ function loginStudent() {
             $content = explode(',',$ligne[$i]);
 
             if($content[0] == $name && $content[1] == $password) {
-                $temp = "Connexion effectuée !";
+                $temp .= "<p style = 'color: #00FF00'> Connexion effectuée </p>";
                 return $temp;
             }
             else {
-                $temp = "Le nom ou le mot de passe est incorrect !";
+                $error = "Le nom ou le mot de passe est incorrect";
                 $i++;
             }
+        }
+        if(isset($error)) {
+            $temp .= "<p style = 'color:#FF0000'> $error </p>";
         }
         return $temp;
     }
 }
+
+////////////////////////////            PROFESSEURS/SECRETAIRES         ////////////////////////////
+
+//Affichage d'un formulaire de connexion pour un professeur/secrétaire
+function formLoginTeacher() {
+    $retour = '<form action="#" method = "post">';
+    $retour .= '<table>';
+    $retour .= '<tr>';
+    $retour .= '<td> <label> Identifiant : </label> </td>';
+    $retour .= '<td> <input type="text" name="id" value=""> </td>';
+    $retour .= '</tr> <tr>';
+    $retour .= '<td> <label> Mot de passe : </label> </td>';
+    $retour .= '<td> <input type="password" name="password" value=""> </td>';
+    $retour .= '</tr>';
+    $retour .= '</table>';
+    $retour .= '<input id="bouton" type="submit" value="Valider" name="submit">';
+    $retour .= '</form>';
+
+    return $retour;
+}
+
+//Authentification d'un professeur/secrétaire
+function loginTeacher() {
+    $temp = "";
+    $lignes = file("../ressources/login_teacher.csv");
+    $i=0;
+    if (isset($_POST['submit'])) {
+        $id = $_POST['id'];
+        $password = $_POST['password'];
+
+        foreach ($lignes as $ligne[]) {
+            $content = explode(',', $ligne[$i]);
+
+            if($content[3] == $id && $content[4] == $password) {
+                $temp .= "<p style = 'color: #00FF00'> Connexion effectuée </p>";
+                return $temp;
+            }
+            else {
+                $error = "L'identifiant ou le mot de passe est incorrect";
+                $i++;
+            }
+        }
+        if(isset($error)) {
+            $temp .= "<p style = 'color:#FF0000'> $error </p>";
+        }
+        return $temp;
+    }
+}
+
+////////////////////////////            ADMINISTRATEUR          ////////////////////////////
 
 //Affichage d'un formulaire de connexion pour un administrateur
 function formLoginAdministrator() {
@@ -62,6 +118,7 @@ function formLoginAdministrator() {
 
 //Authentification d'un administrateur
 function loginAdministrator() {
+    $temp = "";
     $lignes = file("../ressources/login_administrator.csv");
     $i=0;
     if (isset($_POST['submit'])) {
@@ -72,13 +129,16 @@ function loginAdministrator() {
             $content = explode(',', $ligne[$i]);
 
             if($content[3] == $id && $content[4] == $password) {
-                $temp = "Connexion effectuée !";
+                $temp .= "<p style = 'color: #00FF00'> Connexion effectuée </p>";
                 return $temp;
             }
             else {
-                $temp = "L'identifiant ou le mot de passe est incorrect !";
+                $error = "L'identifiant ou le mot de passe est incorrect";
                 $i++;
             }
+        }
+        if(isset($error)) {
+            $temp .= "<p style = 'color:#FF0000'> $error </p>";
         }
         return $temp;
     }
