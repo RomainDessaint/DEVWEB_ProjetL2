fb3cf3948d4b4bf3f92ecae7526ad20b02cfae66
RomainDessaint <romaindessaint@gmail.com>
2018-04-12 23:44:11 +0200

    trua

--- Web Development Project/index.php
+++ Web Development Project/index.php
@@ -9,13 +9,13 @@ echo display_header("ENT - Accueil", "styles/style_index.css");
 <section>
 	<h1> Accueil </h1>
 	<article>
-		<a href="src/etudiants.php"> <h2> ESPACE ETUDIANTS </h2> </a>
+		<a href="src/login_student.php"> <h2> ESPACE ETUDIANTS </h2> </a>
 	</article>
 	<article>
-		<a href="src/professeurs.php"> <h2> ESPACE PROFESSEURS </h2> </a>
+		<a href="src/login_teacher.php"> <h2> ESPACE PROFESSEURS </h2> </a>
 	</article>
 	<article>
-		<a href="src/gestionnaires.php"> <h2> ESPACE GESTIONNAIRES </h2> </a>
+		<a href="src/login_administrator.php"> <h2> ESPACE GESTIONNAIRES </h2> </a>
 	</article>
 </section>
 
